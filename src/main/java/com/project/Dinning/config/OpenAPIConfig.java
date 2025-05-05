355cba7e825ec217e1b649dda56aabd76205cc69
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2025-05-05 15:44:49 -0300

    add OpenAPI config file

---
+++ src/main/java/com/project/Dinning/config/OpenAPIConfig.java
@@ -0,0 +1,45 @@
+package com.project.Dinning.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+  @Bean
+  public OpenAPI myOpenApi() {
+    Server devServer = new Server();
+    devServer.setUrl("http://localhost:8080");
+    devServer.setDescription("Server URL in Development environment");
+
+    Contact contact = new Contact();
+    contact.setName("Dinning API");
+    contact.setEmail("dev.allisonribeiro@gmail.com");
+    contact.setUrl("https://github.com/ribeiroAllison/dinning-api");
+
+    License mitLicense = new License()
+        .name("MIT License")
+        .url("https://choosealicense.com/licenses/mit/");
+
+    Info info = new Info()
+        .title("Dining Review API")
+        .version("1.0")
+        .contact(contact)
+        .description("This API exposes endpoints for managing restaurants and reviews.")
+        .termsOfService("https://www.diningapi.com/terms")
+        .license(mitLicense);
+
+    return new OpenAPI()
+        .info(info)
+        .servers(List.of(devServer));
+
+  }
+
+}
